373fcd7681f5a424cc7d62c9387ee15376dc6c61
Omniuser-C <150850627+Mohamed-Cams@users.noreply.github.com>
2024-04-08 22:57:55 +0000

    Add files via upload

---
+++ Gestionaire/src/gestionaire/Gestionaire.java
@@ -0,0 +1,15 @@
+
+package gestionaire;
+
+import com.login.Loginframe;
+
+public class Gestionaire {
+
+    public static void main(String[] args) {
+
+        //créé un objet fram login
+        Loginframe log1 = new Loginframe();
+        log1.show();
+    }
+    
+}
